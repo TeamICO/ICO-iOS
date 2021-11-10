e8e6866d80f0452db0883e5bf8c3f5c7d8e7841f
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-11-10 16:04:13 +0900

    [ADD] 프로필 뷰 스타일샷 셀 파일 추가

---
+++ ICO-iOS/ICO-iOS/Sources/Home/HomeInStyleLife/InnerVC/Cell/StyleCVC.swift
@@ -0,0 +1,17 @@
+//
+//  StyleCVC.swift
+//  ICO-iOS
+//
+//  Created by 이은영 on 2021/11/10.
+//
+
+import UIKit
+
+class StyleCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
