42e08f5695875506ae24b501f8fe29682b94f88c
doyunnn <73818291+doyunnn@users.noreply.github.com>
2021-11-10 13:33:39 +0900

    [ADD] 마이페이지 필요 파일들 추가

---
+++ ICO-iOS/ICO-iOS/Sources/MyPage/MyPageTVC/MyPageUserInfoTVC/MyPageUserInfoTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageUserInfoTVC.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/10.
+//
+
+import UIKit
+
+class MyPageUserInfoTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ICO-iOS/ICO-iOS/Sources/MyPage/MyPageTVC/MypageMyRecentStyleShotTVC/MypageMyRecentStyleShotCVC/MypageMyRecentStyleShotCVC.swift
@@ -0,0 +1,17 @@
+//
+//  MypageMyRecentStyleShotCVC.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/10.
+//
+
+import UIKit
+
+class MypageMyRecentStyleShotCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
