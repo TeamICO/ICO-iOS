741dbd155d3d3e6b85a5fad73be5675ad841fcb5
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-11-10 20:45:56 +0900

    [ADD] 마이 스타일 추가뷰, survey뷰 파일 추가

---
+++ ICO-iOS/ICO-iOS/Sources/MyStyle/StyleUpload/StyleUploadVC.swift
@@ -0,0 +1,29 @@
+//
+//  StyleUploadVC.swift
+//  ICO-iOS
+//
+//  Created by 이은영 on 2021/11/10.
+//
+
+import UIKit
+
+class StyleUploadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
