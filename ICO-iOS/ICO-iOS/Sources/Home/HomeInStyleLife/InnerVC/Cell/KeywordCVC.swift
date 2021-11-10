db442e4eadd113a23461aeb46fb8bd7127fa358e
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-11-11 01:18:33 +0900

    [ADD] 키워드별 뷰 컬렉션뷰, 셀 추가

---
+++ ICO-iOS/ICO-iOS/Sources/Home/HomeInStyleLife/InnerVC/Cell/KeywordCVC.swift
@@ -0,0 +1,17 @@
+//
+//  KeywordCVC.swift
+//  ICO-iOS
+//
+//  Created by 이은영 on 2021/11/11.
+//
+
+import UIKit
+
+class KeywordCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
