3cf480be15600c3e0731ad9ece7520da6102f46e
doyunnn <73818291+doyunnn@users.noreply.github.com>
2021-11-11 11:38:43 +0900

    [ADD] 좋아요, 알림 뷰컨트롤러 추가

---
+++ ICO-iOS/ICO-iOS/Sources/like/LikeVC.swift
@@ -0,0 +1,19 @@
+//
+//  LikeVC.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/11.
+//
+
+import UIKit
+
+class LikeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
