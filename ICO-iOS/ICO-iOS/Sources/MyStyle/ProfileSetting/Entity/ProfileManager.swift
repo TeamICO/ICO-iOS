3e7979414c87197a8438924f764799863318c58c
doyunnn <73818291+doyunnn@users.noreply.github.com>
2021-11-16 21:52:47 +0900

    [ADD] 프로필 수정 조회 기본 파일추가

---
+++ ICO-iOS/ICO-iOS/Sources/MyStyle/ProfileSetting/Entity/ProfileManager.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileManager.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/16.
+//
+
+import Foundation
+import Alamofire
+final class ProfileManager{
+    static let shared = ProfileManager()
+    private init() {}
+    
+    
+    func getUserProfile(userIdx: Int,jwtToken: String, completion: @escaping (ProfileResult)->Void) {
+
+        let url = "https://dev.chuckwagon.shop/app/users/\(userIdx)"
+
+   
+        let header : HTTPHeaders = [
+            "X-ACCESS-TOKEN" : jwtToken
+        ]
+
+
+        AF.request(url,
+                   method: .get,
+                   parameters: nil,
+                   encoding: JSONEncoding.default,
+                   headers: header)
+            .responseDecodable(of: ProfileResponse.self) { response in
+                
+                switch response.result {
+                
+                case .success(let response):
+                    guard response.isSuccess == true else{
+                        return
+                    }
+                    completion(response.result)
+                    
+                    
+                    
+                case .failure(let error):
+                    print("DEBUG>> getUserProfile Get Error : \(error.localizedDescription)")
+                    
+                }
+            }
+        
+    }
+}
+
---
+++ ICO-iOS/ICO-iOS/Sources/MyStyle/ProfileSetting/Entity/ProfileResponse.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileResponse.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/16.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct ProfileResponse: Codable {
+    let isSuccess: Bool
+    let code: Int
+    let message: String
+    let result: ProfileResult
+}
+
+// MARK: - Result
+struct ProfileResult: Codable {
+    let profileURL: String
+    let nickname, resultDescription: String
+    let ecoKeyword: [ProfileEcoKeyword]
+
+    enum CodingKeys: String, CodingKey {
+        case profileURL = "profileUrl"
+        case nickname
+        case resultDescription = "description"
+        case ecoKeyword
+    }
+}
+
+// MARK: - EcoKeyword
+struct ProfileEcoKeyword: Codable {
+    let ecoKeywordIdx: Int
+    let name, type: String
+    let status: EcoStatus
+}
+
+enum EcoStatus: String, Codable {
+    case n = "N"
+    case y = "Y"
+}
