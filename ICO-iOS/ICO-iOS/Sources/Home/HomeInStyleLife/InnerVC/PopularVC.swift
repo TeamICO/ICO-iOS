b4c6f8e2c9bd9db6d2328f47ec64e511ce03e521
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-11-18 00:09:15 +0900

    [FIX] 라이프스타일 셀 클릭했을때 색깔 안변하도록 수정

--- ICO-iOS/ICO-iOS/Sources/Home/HomeInStyleLife/InnerVC/PopularVC.swift
+++ ICO-iOS/ICO-iOS/Sources/Home/HomeInStyleLife/InnerVC/PopularVC.swift
@@ -60,6 +60,8 @@ extension PopularVC: UITableViewDelegate , UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecentTVC", for: indexPath)as? RecentTVC else {return UITableViewCell()}
         
+        cell.selectionStyle = .none
+        
         return cell
     }
     
