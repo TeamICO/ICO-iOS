82a6952381008bc42cd5d4768472c3fe189e13c8
doyunnn <73818291+doyunnn@users.noreply.github.com>
2021-11-18 20:31:03 +0900

    [FEAT] 알람 목록 조회 및 읽기 API 연동

---
+++ ICO-iOS/ICO-iOS/Sources/Alarm/Entity/AlarmManager.swift
@@ -0,0 +1,84 @@
+//
+//  AlarmManager.swift
+//  ICO-iOS
+//
+//  Created by do_yun on 2021/11/18.
+//
+
+import Foundation
+import Alamofire
+final class AlarmManager{
+    static let shared = AlarmManager()
+    private init() {}
+    
+  
+    func getUserAlarms(jwtToken: String, completion: @escaping (AlarmResult)->Void) {
+
+        let url = "https://dev.chuckwagon.shop/app/notifications"
+
+   
+        let header : HTTPHeaders = [
+            "X-ACCESS-TOKEN" : jwtToken
+        ]
+
+
+        AF.request(url,
+                   method: .get,
+                   parameters: nil,
+                   encoding: JSONEncoding.default,
+                   headers: header)
+            .responseDecodable(of: AlarmResponse.self) { response in
+                
+                switch response.result {
+                
+                case .success(let response):
+                    guard response.isSuccess == true else{
+                        return
+                    }
+                    
+                    completion(response.result)
+                    
+                    
+                    
+                case .failure(let error):
+                    print("DEBUG>> getUserAlarms Get Error : \(error.localizedDescription)")
+                    
+                }
+            }
+        
+    }
+    func readAlarm(type: String,notificationIdx : Int,jwtToken: String, completion: @escaping (Bool)->Void) {
+
+        let url = "https://dev.chuckwagon.shop/app/notifications/\(notificationIdx)"
+
+   
+        let header : HTTPHeaders = [
+            "X-ACCESS-TOKEN" : jwtToken
+        ]
+        let param  = [
+            "isNew" : "N",
+            "type" : type
+        ]
+
+        AF.request(url,
+                   method: .patch,
+                   parameters: param,
+                   encoding: JSONEncoding.default,
+                   headers: header)
+            .responseDecodable(of: CommonResponse.self) { response in
+                
+                switch response.result {
+                
+                case .success(let response):                    
+                    completion(response.isSuccess)
+                    
+                    
+                    
+                case .failure(let error):
+                    print("DEBUG>> readAlarm Get Error : \(error.localizedDescription)")
+                    
+                }
+            }
+        
+    }
+}
