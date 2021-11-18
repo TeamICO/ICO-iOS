6074ce08ff07f6dc95272b5fe55faaa1fcf86c90
eunyoung <70764912+eunyoung23@users.noreply.github.com>
2021-11-18 13:33:57 +0900

    [FIX] 인기있는 아이코셀 테두리 그라데이션 컬러 적용

--- ICO-iOS/ICO-iOS/Configuration/Extenstion/UIImageView.swift
+++ ICO-iOS/ICO-iOS/Configuration/Extenstion/UIImageView.swift
@@ -9,7 +9,7 @@ import UIKit
 import Kingfisher
 
 extension UIImageView {
-    
+    //서버에서 이미지 가져오기
     func setImage(with urlString: String) {
         let cache = ImageCache.default
         cache.retrieveImage(forKey: urlString, options: nil) { result in // 캐시에서 키를 통해 이미지를 가져온다.
@@ -28,6 +28,7 @@ extension UIImageView {
         }
     }
     
+    //서버에서 받아온 이미지 cornerRadius적용하기
     func download(url: String?, rounded: Bool = true) {
         guard let _url = url else {
             return
@@ -38,5 +39,31 @@ extension UIImageView {
         } else {
             self.kf.setImage(with: URL(string: _url))
         }
+    }
+    
+    //UIImageView에 border에 그라데이션 적용하기
+    func addCircleGradiendBorder(_ width: CGFloat) {
+        let gradient = CAGradientLayer()
+        gradient.frame =  CGRect(origin: CGPoint.zero, size: bounds.size)
+        let colors: [CGColor] = [UIColor.init(red: 23/255, green: 170/255, blue: 107/255, alpha: 1.0).cgColor,
+                                 UIColor.init(red: 179/255, green: 242/255, blue: 160/255, alpha: 1.0).cgColor]
+        gradient.colors = colors
+        gradient.startPoint = CGPoint(x: 1, y: 0.5)
+        gradient.endPoint = CGPoint(x: 0, y: 0.5)
+        
+        let cornerRadius = frame.size.width / 2
+        layer.cornerRadius = cornerRadius
+        clipsToBounds = true
+        
+        let shape = CAShapeLayer()
+        let path = UIBezierPath(ovalIn: bounds)
+        
+        shape.lineWidth = width
+        shape.path = path.cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor // clear
+        gradient.mask = shape
+        
+        layer.insertSublayer(gradient, below: layer)
     }
 }
